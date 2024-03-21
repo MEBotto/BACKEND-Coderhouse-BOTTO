8a2cfd2d21113e32b6fc658ac06d41dc3ef8e5ce
Mariano Botto <marianobotto@gmail.com>
2024-03-21 14:11:04 -0300

    fix - back: Add category and status field

--- Backend/src/models/product.model.js
+++ Backend/src/models/product.model.js
@@ -2,15 +2,22 @@ import { Schema, model } from "mongoose";
 import mongoosePaginate from "mongoose-paginate-v2";
 
 const productSchema = new Schema({
-  title: {type: String, required: true},
-  description: {type: String, required: true},
-  price: {type: Number, required: true},
-  thumbnail: {type: String, required: true},
-  code: {type: String, required: true, unique: true},
-  stock: {type: Number, required: true},
-})
-productSchema.plugin(mongoosePaginate)
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  thumbnail: { type: String, required: true },
+  code: { type: String, required: true, unique: true },
+  stock: { type: Number, required: true },
+  category: {
+    type: String,
+    required: true,
+    enum: ["Comics", "Manga", "Fantasy", "Romance", "Other"],
+  },
+  status: { type: Boolean, required: true },
+});
 
-const productModel = model("products", productSchema)
+productSchema.plugin(mongoosePaginate);
 
-export { productModel }
+const productModel = model("products", productSchema);
+
+export { productModel };
