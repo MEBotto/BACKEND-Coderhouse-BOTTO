0da493736dd652e05a2acce66b53848bb83cc73b
Mariano Botto <marianobotto@gmail.com>
2024-03-22 00:13:56 -0300

    fix - back: getUserById route

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -74,6 +74,6 @@ authRouter.put("/user/:id", updateAccountController);
 
 authRouter.get("/users", checkUserRole(["ADMIN"]), getAllUsersController);
 
-authRouter.get("/userById/:id", getUserByIdController);
+authRouter.get("/user/:id", getUserByIdController);
 
 export default authRouter;
