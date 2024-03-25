b2749fb757eba85ca62ee462fcd1abe651ca34d7
Mariano Botto <marianobotto@gmail.com>
2024-03-25 10:35:04 -0300

    fix - front: Delete features non used

--- Frontend/src/pages/Login/Login.jsx
+++ Frontend/src/pages/Login/Login.jsx
@@ -7,63 +7,7 @@ import { useTheme } from "../../context/ThemeContext.jsx";
 import FormLogin from "../../components/Forms/FormLogin.jsx";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [loggedIn, setLoggedIn] = useState(false);
-  const { login } = useAuth();
   const { theme } = useTheme();
-  const navigate = useNavigate();
-
-  const handleLogin = async () => {
-    try {
-      const response = await fetch(
-        "http://localhost:8080/api/extend/users/login",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            email: email,
-            password: password,
-          }),
-        }
-      );
-
-      if (response.status === 200) {
-        const data = await response.json();
-        login(data.access_token, data.payload);
-        Swal.fire({
-          position: "center",
-          icon: "success",
-          title: "Successful Login",
-          showConfirmButton: false,
-          timer: 1500,
-        });
-        setLoggedIn(true);
-      } else {
-        Swal.fire({
-          icon: "error",
-          title: "Oops...",
-          text: "Credentials don't match",
-          footer: `Status Code: ${response.status} - ${response.statusText}`,
-        });
-      }
-    } catch (error) {
-      Swal.fire({
-        icon: "error",
-        title: "Oops...",
-        text: "ERROR!",
-        footer: `Error making the request: ${error.message}`,
-      });
-    }
-  };
-
-  if (loggedIn) {
-    setTimeout(() => {
-      navigate("/products");
-    }, 2000);
-  }
 
   return (
     <div
