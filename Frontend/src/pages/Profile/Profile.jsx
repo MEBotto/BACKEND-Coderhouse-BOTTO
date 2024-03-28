a0920fc951a9e2bfdbda267b1b71c2f02f6e8343
Mariano Botto <marianobotto@gmail.com>
2024-03-28 12:24:57 -0300

    feat - front: Add jwtDecode

--- Frontend/src/pages/Profile/Profile.jsx
+++ Frontend/src/pages/Profile/Profile.jsx
@@ -1,39 +1,51 @@
 import { useEffect, useState } from "react";
 import { useAuth } from "../../context/AuthContext.jsx";
 import { useTheme } from "../../context/ThemeContext.jsx";
+import { jwtDecode } from "jwt-decode";
 
 const Profile = () => {
-  const [userData, setUserData] = useState({
-    photo: "https://avatars.githubusercontent.com/u/106409126?v=4",
-    name: "Mariano Eloy Botto",
-    email: "marianobotto92@gmail.com",
-    role: "user",
-  });
+  const [userData, setUserData] = useState(null);
   const { token } = useAuth();
   const { theme } = useTheme();
 
+  useEffect(() => {
+    const decodedToken = jwtDecode(token);
+    setUserData(decodedToken.user);
+    console.log(userData);
+  }, []);
+
   return (
-    <div className="mt-20 p-5">
-      <h1 className="text-4xl uppercase font-bold mb-5">User Profile</h1>
-      {userData ? (
-        <div className="grid grid-cols-4">
-          <div className="col-span-1">
-            <img
-              src={userData.photo}
-              alt={`Avatar of ${userData.name}`}
-              className="rounded-full"
-            />
-          </div>
-          <div className="col-span-3">
-            <p>Name: {userData.name}</p>
-            <p>Email: {userData.email}</p>
-            <p>Age: {userData.age}</p>
-            <p>Role: {userData.role}</p>
-          </div>
-        </div>
-      ) : (
-        <p>No user data available. Please log in.</p>
-      )}
+    <div
+      className={`w-screen h-screen p-5 pt-20 ${
+        theme === "dark" ? "text-white bg-color" : "text-black bg-colorLight"
+      }`}
+    >
+      <div className={`p-5`}>
+        {userData ? (
+          <>
+            <h1 className={`text-4xl uppercase font-bold mb-5`}>
+              {userData.name} - Profile
+            </h1>
+            <div className="grid grid-cols-4">
+              <div className="col-span-1">
+                <img
+                  src={userData.photo}
+                  alt={`Avatar of ${userData.name}`}
+                  className="rounded-full"
+                />
+              </div>
+              <div className="col-span-3">
+                <p>Name: {userData.name}</p>
+                <p>Email: {userData.email}</p>
+                <p>Age: {userData.age}</p>
+                <p>Role: {userData.role}</p>
+              </div>
+            </div>
+          </>
+        ) : (
+          <p>No user data available. Please log in.</p>
+        )}
+      </div>
     </div>
   );
 };
