51ac1d0c35c6ab64606019d28becb35b529ab9a7
Mariano Botto <marianobotto@gmail.com>
2024-04-23 19:44:47 -0300

    feat - front: Add services section

---
+++ Frontend/src/components/ServicesSection.jsx
@@ -0,0 +1,81 @@
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+export default function ServicesSection({ theme }) {
+  return (
+    <section className="w-full">
+      <div
+        className={`container mx-auto p-4 ${
+          theme === "dark" ? "text-white" : "text-black"
+        }`}
+      >
+        <h2 className="text-4xl text-center font-bold mb-2">
+          Welcome to the best online store
+        </h2>
+        <p className="text-center text-lg">
+          Here you can find the best products at the best price
+        </p>
+      </div>
+      <div
+        className={`grid sm:max-lg:grid-rows-3 lg:grid-cols-3 divide-y lg:divide-x lg:divide-y-0 ${
+          theme === "dark"
+            ? "divide-zinc-400 text-white"
+            : "divide-zinc-600 text-black"
+        } divide-gray-200`}
+      >
+        <div className="flex flex-col lg:flex-row items-center justify-center gap-2 p-4">
+          <i className="ri-truck-line text-6xl hover:scale-110 transition-transform duration-200" />
+          <div className="flex flex-col items-center lg:items-start">
+            <h3 className="font-bold text-xl underline underline-offset-2">
+              We ship to your door!
+            </h3>
+            <p
+              className={`${
+                theme === "dark" ? "text-zinc-300" : "text-zinc-500"
+              }`}
+            >
+              wherever you are!
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col lg:flex-row items-center justify-center gap-2 p-4">
+          <i className="ri-secure-payment-line text-6xl hover:scale-110 transition-transform duration-200" />
+          <div className="flex flex-col items-center lg:items-start">
+            <h3 className="font-bold text-xl underline underline-offset-2">
+              Pay online!
+            </h3>
+            <p
+              className={`${
+                theme === "dark" ? "text-zinc-300" : "text-zinc-500"
+              }`}
+            >
+              with MercadoPago
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col lg:flex-row items-center justify-center gap-2 p-4">
+          <i className="ri-chat-1-line text-6xl hover:scale-110 transition-transform duration-200" />
+          <div className="flex flex-col items-center lg:items-start">
+            <Link
+              to={"/chat"}
+              className="font-bold text-xl underline underline-offset-2"
+            >
+              Use our chat!
+            </Link>
+            <p
+              className={`${
+                theme === "dark" ? "text-zinc-300" : "text-zinc-500"
+              }`}
+            >
+              chat with the community
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+ServicesSection.propTypes = {
+  theme: PropTypes.string.isRequired,
+};
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,21 +1,20 @@
 import useTheme from "../hooks/useTheme.js";
 import Carousel from "../components/Carousel.jsx";
+import ServicesSection from "../components/ServicesSection.jsx";
 
 export default function Home() {
   const { theme } = useTheme();
 
   return (
     <>
-      <Carousel theme={theme}/>
+      <Carousel theme={theme} />
       <div
         className={`${
           theme === "dark" ? "bg-color" : "bg-colorLight"
         } w-full h-full flex flex-col justify-center items-center p-8`}
       >
-        <section className="w-full bg-green-50 ">
-          asddddddddddddddd
-        </section>
-      </div>  
+        <ServicesSection theme={theme} />
+      </div>
     </>
   );
 }
