ab524bb1d5cebe5dd4055284c32df4f3c6b8aebc
MEBotto <marianobotto92@gmail.com>
2024-04-27 21:02:51 -0300

    style - front: Add responsive to not logged in view

--- Frontend/src/pages/Cart.jsx
+++ Frontend/src/pages/Cart.jsx
@@ -111,21 +111,23 @@ const Cart = () => {
           </div>
         )
       ) : (
-        <div className="w-3/4 h-fit p-5 border border-gray-400 rounded-lg flex flex-col justify-center items-center">
-          <h1 className="text-bold text-3xl my-4">
-            Oops! It looks like you are not logged in.
-          </h1>
-          <h2 className="my-2">Please log in to view the cart</h2>
-          <Link to={"/login"}>
-            <Button
-              text={"Go to login"}
-              className={`${
-                theme === "dark"
-                  ? "bg-mainColor text-black"
-                  : "bg-mainColorLight text-white"
-              } p-2 rounded-xl font-bold mt-6`}
-            />
-          </Link>
+        <div className="w-full h-minusNavbar p-4 flex justify-center items-center">
+          <div className="h-fit border border-gray-400 rounded-lg flex flex-col justify-center items-center p-4">
+            <h1 className="text-bold text-3xl my-4 text-center">
+              Oops! It looks like you are not logged in.
+            </h1>
+            <h2 className="my-2 text-center">Please log in to view the cart</h2>
+            <Link to={"/login"}>
+              <Button
+                text={"Go to login"}
+                className={`${
+                  theme === "dark"
+                    ? "bg-mainColor text-black"
+                    : "bg-mainColorLight text-white"
+                } p-2 rounded-xl font-bold mt-6`}
+              />
+            </Link>
+          </div>
         </div>
       )}
     </div>
