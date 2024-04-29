29f73d7e2ba5e18648d986edb085109b91283f9c
MEBotto <marianobotto92@gmail.com>
2024-04-29 12:02:55 -0300

    feat - back: Add first_name, last_name, documents and last_connection fields

--- Backend/src/services/dto/user.dto.js
+++ Backend/src/services/dto/user.dto.js
@@ -1,9 +1,12 @@
 export default class UserDto {
   constructor(user) {
-    this.name = user.first_name + user.last_name
+    this.first_name = user.first_name
+    this.last_name = user.last_name
     this.age = user.age;
     this.email = user.email;
     this.role = user.role;
     this.photo = user.photo;
+    this.documents = user.documents;
+    this.last_connection = user.last_connection;
   }
 }
