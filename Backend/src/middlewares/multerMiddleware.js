e8ffa7ef117fb442f4726c647f67af9a70b1ceaf
MEBotto <marianobotto92@gmail.com>
2024-04-28 21:28:48 -0300

    feat - back: Add verification of documents to multerMiddleware

--- Backend/src/middlewares/multerMiddleware.js
+++ Backend/src/middlewares/multerMiddleware.js
@@ -2,6 +2,7 @@ import multer from "multer";
 import logger from "../utils/logger.js";
 import { config } from "../config/env.config.js";
 import { v2 as cloudinary } from "cloudinary";
+import { authService } from "../services/factory.js";
 
 cloudinary.config({
   cloud_name: config.cloudinaryCloudName,
@@ -117,7 +118,16 @@ export const uploadToCloudinary = (fieldName, uploadType) => {
             bankStatementProof: bankStatementProof[0],
           };
 
+          const { uid } = req.params;
+          const { documents } = await authService.getAccountById(uid);
+
           for (const [fileKey, file] of Object.entries(files)) {
+            let arrayDocuments = Array.isArray(documents)
+              ? documents
+              : [documents];
+            if (arrayDocuments.some((doc) => doc.file.includes(fileKey))) {
+              continue;
+            }
             const b64 = Buffer.from(file.buffer).toString("base64");
             let dataURI = "data:" + file.mimetype + ";base64," + b64;
             const originalname = file.originalname
@@ -135,6 +145,13 @@ export const uploadToCloudinary = (fieldName, uploadType) => {
               folder: "uploads",
             });
             uploadResponses.push({ ...response, file: fileKey });
+          }
+          
+          if (uploadResponses.length === 0) {
+            return res.status(400).json({
+              success: false,
+              error: "All 3 necessary files have already been uploaded.",
+            });
           }
 
           req.cloudinaryUploads = uploadResponses.map((response) => ({
