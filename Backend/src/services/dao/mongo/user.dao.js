bf55673ae41f59d119e417ee9b8ec5663f908c5b
MEBotto <marianobotto92@gmail.com>
2024-04-29 14:42:05 -0300

    feat - back: Create user.dao.js and move some methods from auth.dao

---
+++ Backend/src/services/dao/mongo/user.dao.js
@@ -0,0 +1,20 @@
+import userModel from "../../../models/user.model";
+
+export default class UserDAO {
+  constructor() {}
+  getAll = async () => {
+    return await userModel.find();
+  };
+  getAccountById = async (id) => {
+    return await userModel.findOne({ _id: id });
+  };
+  getAccountByEmail = async (email) => {
+    return await userModel.findOne({ email: email });
+  };
+  updateAccount = async (id, account) => {
+    return await userModel.findByIdAndUpdate(id, account);
+  };
+  deleteAccount = async (id) => {
+    return await userModel.findByIdAndDelete(id);
+  };
+}
